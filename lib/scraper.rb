4b9255ba628c059b71c0812f205f597a74d87fc0
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-08 20:17:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open('https://learn-co-curriculum.github.io/student-scraper-test-page/index.html')
+    doc = Nokogiri::HTML(html)
+    cards = doc.css('div.student-card a').text.each do |student_card|
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
     
   end
-
 end
-
